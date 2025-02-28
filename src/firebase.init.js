127064ad610419ae07db6f5b5d701d8d6d4c995c
km-rakib <kazimdrakibulhassan@gmail.com>
2025-02-28 23:48:10 +0600

    installed firebase and set router

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZKwy1ueRXuF95Nlk1st4IRXXa0xZW9NU",
+  authDomain: "auth-milon-773a4.firebaseapp.com",
+  projectId: "auth-milon-773a4",
+  storageBucket: "auth-milon-773a4.firebasestorage.app",
+  messagingSenderId: "886743580543",
+  appId: "1:886743580543:web:b3a7a54a01891bb9ace44a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
