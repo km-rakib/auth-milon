6fc30072604a54a8d1af08275bc20102fa757c55
km-rakib <kazimdrakibulhassan@gmail.com>
2025-03-03 23:30:59 +0600

    auth context

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,24 +1,9 @@
-// import React, {createContext} from 'react';
-// const AuthContext = createContext(null);
-// const AuthProvider = ({children}) => {
-
-//   const authInfo = {
-//     name: "Nodi"
-//   }
-//   return (
-//     <AuthContext.Provider value={authInfo}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export default AuthProvider;
-
-import React, { createContext } from 'react';
+import React, {createContext} from 'react';
 const AuthContext = createContext(null);
 const AuthProvider = ({children}) => {
-  authInfo = {
-    name: "Komol"
+
+  const authInfo = {
+    name: "Nodi"
   }
   return (
     <AuthContext.Provider value={authInfo}>
@@ -28,3 +13,18 @@ const AuthProvider = ({children}) => {
 };
 
 export default AuthProvider;
+
+// import React, { createContext } from 'react';
+// const AuthContext = createContext(null);
+// const AuthProvider = ({children}) => {
+//   authInfo = {
+//     name: "Komol"
+//   }
+//   return (
+//     <AuthContext.Provider value={authInfo}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export default AuthProvider;
