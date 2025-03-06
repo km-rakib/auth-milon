333141c865de55d3bcb7d122b88b6dde1e7e5d8d
km-rakib <kazimdrakibulhassan@gmail.com>
2025-03-06 23:58:48 +0600

    formated

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,27 +1,24 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, {createContext} from 'react';
-import { auth } from '../firebase.init';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { createContext } from "react";
+import { auth } from "../firebase.init";
 export const AuthContext = createContext(null);
-const AuthProvider = ({children}) => {
-  const name = "Alu Mia"
-  const age = 60
+const AuthProvider = ({ children }) => {
+  const name = "Alu Mia";
+  const age = 60;
 
   const createUser = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password)
-  }
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
   const authInfo = {
-    name,age,
-    createUser
-  }
+    name,
+    age,
+    createUser,
+  };
   return (
-    <AuthContext.Provider value={authInfo}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
-
-
 
 export default AuthProvider;
 
