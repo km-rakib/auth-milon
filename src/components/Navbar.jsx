627066090bf27b39275e9ac9ca110d230bba781d
km-rakib <kazimdrakibulhassan@gmail.com>
2025-03-07 23:21:39 +0600

    gave the nav a margin align to hero and footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   console.log(name)
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 px-28">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
