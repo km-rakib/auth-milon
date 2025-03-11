c6e4404cd4e23317705db3b9a73ccfb3167cfa36
km-rakib <kazimdrakibulhassan@gmail.com>
2025-03-11 22:08:01 +0600

    indentation fixed

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ( {children} ) => {
+const PrivateRoute = ({children}) => {
 
 
   const {user, loading} = useContext(AuthContext)
